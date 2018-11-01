ce7f7a6ec6c29a254d7d292c58bbb9fce2b1af1f
daisuke matsumoto <daisuke.matsumoto@daisukenoMacBook-Air.local>
2018-11-01 15:22:35 +0900

    修正点の変更
    コメントアウトの削除
    current_userを取得しない記述
    valueの変更
    ハッシュロケットの記述変更
    Prefixのパスに変更

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -16,10 +16,6 @@ $(function() {
                   <p class='chat-group-user__name'>${ name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
-     // `<div class="chat-group-user clearfix">
-     //              <p class="chat-group-user__name">${ user.name }</p>
-     //              <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">削除</a>
-     //            </div>`
     return html;
   }
   function appendNoUser(user) {
