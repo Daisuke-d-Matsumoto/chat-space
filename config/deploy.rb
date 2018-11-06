c634edf5683d0d8adc5a27e511d3330ca2600da3
daisuke matsumoto <daisuke.matsumoto@daisukenoMacBook-Air.local>
2018-11-06 16:15:36 +0900

    eploy.rbの記述変更
    deploy.rbの足りない記述を追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,12 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
-set :linked_files, %w{ config/secrets.yml }
 set :application, "chat-space"
 set :repo_url, "git@example.com:Daisuke-d-Matsumoto/chat-space.git"
 
@@ -16,6 +21,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
