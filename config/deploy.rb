e60d2f624294344a676ec3bbf282ed04703ad8f7
daisuke matsumoto <>
2018-11-06 19:56:22 +0900

    Change repo_url in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :default_env, {
 }
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:Daisuke-d-Matsumoto/chat-space.git"
+set :repo_url, "git@github.com:Daisuke-d-Matsumoto/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
