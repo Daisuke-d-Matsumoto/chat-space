e53a74c40e38579fe94bb82e183d0ff43e4e2c7c
daisuke matsumoto <>
2018-11-06 20:02:42 +0900

    Change ssh_options :keys in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/matsumoto.d/.ssh/daisukem.pem']
+                  keys: ['/Users/daisuke.matsumoto/.ssh/daisukem.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
