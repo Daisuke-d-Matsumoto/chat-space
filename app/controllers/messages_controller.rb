2b1d101fb58bc2b5dcc05233bdbd2c5c5fcf61b1
daisuke matsumoto <daisuke.matsumoto@daisukenoMacBook-Air.local>
2018-11-07 14:52:21 +0900

    Delete html responce

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,6 @@ class MessagesController < ApplicationController
     @message.user = current_user
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
       end
     else
